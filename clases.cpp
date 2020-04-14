abb1eda5d9a3d078be7ec54d81b3676573f18936
erosCarhuancho <55807262+erosCarhuancho@users.noreply.github.com>
2020-04-14 08:30:38 -0500

    clases.cpp

--- clases.cpp
+++ clases.cpp
@@ -10,15 +10,22 @@ Integer::Integer(const Integer& other)
   value= other.value;
 };
 
+Integer::operator int()
+{
+  return this->value;
+}
+
 Integer Integer :: operator=(const Integer& other)
 {
+  this->value= other.value;
+  return *this;
 };
 
 
 Integer Integer:: operator+(int value)
 {
-}
-;
+  return this->value+value;
+};
 
 std::ostream& operator <<(std::ostream& os, const Integer& a)
  {
